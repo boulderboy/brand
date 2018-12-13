cb1f7cb019f8d27e0cd53036b27e5581974582ca
boulderboy <gl.andrewmess@gmail.com>
2018-12-13 14:47:21 +0200

    отрисовка каталога

---
+++ php/catalog-render.php
@@ -0,0 +1,25 @@
+<?php
+$link = mysqli_connect("host-2", "root", "", "brand");
+$query = "select * from goods";
+
+$result = mysqli_query($link, $query);
+
+while ($info = mysqli_fetch_assoc($result)){
+    echo '<div class="product-hover">
+                <a href="single-page.html" class="products__card">
+                <img src="../'.$info["path-to-img"].'" alt="">
+                 <div class="products__text">
+               <p>'.$info["name"].'</p>
+               <span>$'.$info["price"].'</span>
+                </div>
+                </a>
+               
+              
+                <div class="add-flex">
+                <a href="#" class="add-to-cart">add to cart</a>
+                </div>
+          </div>';
+}
+
+
+
