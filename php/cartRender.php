06ce1ac7cb05329c733a19d0955ae3634f87d1ee
boulderboy <gl.andrewmess@gmail.com>
2018-12-17 15:27:51 +0200

    отрисовка корзины в php

--- php/cartRender.php
+++ php/cartRender.php
@@ -1,7 +1,35 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: andreygladyshev
- * Date: 17.12.2018
- * Time: 14:35
- */
+session_start();
+
+$goodId = $_POST['id'];
+
+if(isset($_SESSION[$goodId])){
+    $_SESSION[$goodId]['quantity'] = $_SESSION[$goodId]['quantity'] + 1;
+}
+else {
+    $link = mysqli_connect("host-2", "root", "", "brand");
+    $query = "select * from goods where id = $goodId";
+    $data = mysqli_query($link, $query);
+    $good = [];
+
+    while ($data = mysqli_fetch_assoc($data)) {
+        $good['name'] = $data['name'];
+        $good['price'] = $data['price'];
+        $good['rating'] = $data['rating'];
+        $good['quantity'] = 1;
+    };
+    $_SESSION[$goodId] = $good;
+}
+
+
+function cartRender(){
+    if(!isset($_SESSION["goods"])){
+        echo "<span>Your cart is empty</span>";
+    } else  {
+        foreach ($_SESSION[goods] as $prodInfo){
+            foreach ($prodInfo as $a => $info){
+                print_r($info);
+            }
+        }
+    }
+}
